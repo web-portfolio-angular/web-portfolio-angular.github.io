610e3790b8d2ea74e4ac53003997e7b1b60939e0
web-portfolio-angular <atanas.ivanov1808@gmail.com>
2020-09-18 00:04:02 +0300

    nodemailer

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+const admin = require ('firebase-admin');
+const nodemailer = require('nodemailer');
+
+admin.initializeApp();
+require('dotenv').config()
+
+const {SENDER_EMAIL, SENDER_PASSWORD} = process.env;
+
+exports.sendEmailNotification = functions.firestore.document('submissions/{docId}')
+.onCreate((snap) => {
+  const data = snap.data();
+
+  let authData = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: SENDER_EMAIL,
+      pass: SENDER_PASSWORD
+    }
+  })
+
+  authData.sendMail({
+    from: 'submissions@gmail.com',
+    to: `${data.email}`,
+    subject: 'Submission Info',
+    text: `${data.email}`,
+    html: `${data.email}`,
+  }).then(() => console.log('The email is sent successfully')).catch(err => console.log(err));
+});
+
