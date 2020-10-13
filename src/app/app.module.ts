e0d456f784ce99fa07a7119ba7b735760678d82c
bookies-images <atanas.ivanov@efbet.com>
2020-10-13 17:47:26 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ export class AppModule {}
 // loading spinner sass
 // da opravq theme switcha i servica
 // textarea place holder color
+// change color names
 
 //  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" rel="stylesheet">
 //  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
