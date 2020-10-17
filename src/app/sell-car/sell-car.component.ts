d7d66223af615559772c954f67bfcf3d47bdea82
web-portfolio-angular <atanas.ivanov1808@gmail.com>
2020-10-17 15:45:20 +0300

    update

--- src/app/sell-car/sell-car.component.ts
+++ src/app/sell-car/sell-car.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-sell-car',
@@ -8,14 +8,12 @@ import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage
   ]
 })
 export class SellCarComponent implements OnInit {
-  imgPath;
+  file;
   imgName: string;
-  defaultUploadImg = '../assets/img/cell-car/upload-img.png';
-  imgLocalPath = this.defaultUploadImg;
-  errorMsgOnUpload = null;
-
-  task: AngularFireUploadTask;
-  imgURL;
+  defaultUploadImg:string = '../assets/img/cell-car/upload-img.png';
+  imgLocalPath:string = this.defaultUploadImg;
+  imgURL:string = null;
+  errorMsgOnUpload = null;  
 
   constructor(private angularFireStorage: AngularFireStorage) { }
 
@@ -23,22 +21,26 @@ export class SellCarComponent implements OnInit {
   }
 
   uploadImg(event: any){
-    this.imgPath = event.target.files[0];  
-    if (this.imgPath) {
+    this.file = event.target.files[0];  
+    if (this.file) {
       const reader = new FileReader();
       reader.onload = (event: any) => {
           this.imgLocalPath = event.target.result;
       }
       reader.readAsDataURL(event.target.files[0]);
-      this.imgName = this.imgPath.name.substr(0, this.imgPath.name.lastIndexOf('.'));
+      this.imgName = this.file.name.substr(0, this.file.name.lastIndexOf('.'));
     } else {
       this.imgLocalPath = this.defaultUploadImg;
-    }     
+    }    
   }
 
   uploadImgToFirestorage(){
-    this.angularFireStorage.upload("/cellCar/" + this.imgName + "-" + Math.random(), this.imgPath)
-    .then(() => {
+    this.angularFireStorage.upload("/cellCar/" + this.imgName + "-" + Math.random().toString(36).substring(2), this.file)
+    .then(uploadTask => {
+      uploadTask.ref.getDownloadURL().then(url => {
+        this.imgURL = url;
+        console.log(url);        
+      })      
       this.errorMsgOnUpload = null;    
     })
     .catch(error => {
