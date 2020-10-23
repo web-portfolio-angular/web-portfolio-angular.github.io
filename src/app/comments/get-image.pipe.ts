f3180c4a79682cae3c6109c29ffed6d8162f9651
bookies-images <atanas.ivanov@efbet.com>
2020-10-23 12:13:56 +0300

    update

--- src/app/comments/get-image.pipe.ts
+++ src/app/comments/get-image.pipe.ts
@@ -1,22 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { map } from 'rxjs/operators';
+
 import { FirestoreService } from '../shared/services/firestore.service';
 import { UserAdditionalInfo } from '../shared/models/user-additional-info.model';
 
-@Pipe({name: 'getImage'})
+@Pipe({ name: 'getImage' })
 export class GetImagePipe implements PipeTransform {
 
-  constructor(private firestore: FirestoreService){}
-  
-  transform(value: string){
-    this.firestore.getRegistration(value).subscribe(ressData => {
+  constructor(private firestore: FirestoreService) { }
+
+  transform(value: string) {
+    return this.firestore.getRegistration(value).pipe(map(ressData => {
       const userAdditionalData = ressData.map(e => {
         return {
           id: e.payload.doc.id,
           ...e.payload.doc.data() as UserAdditionalInfo
         }
       })
-      console.log(userAdditionalData[0].userImg);
       return userAdditionalData[0].userImg;
-    })    
+    }))
   }
 }
