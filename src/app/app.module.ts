2bbc5f8d64e22d27c2d98dd79268ac5d1f12161e
web-portfolio-angular <atanas.ivanov1808@gmail.com>
2020-10-29 11:43:05 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,15 +39,8 @@ export class AppModule {}
 // temata prezapisva transitiona na drugite elementi pri hover
 // loading spinner sass
 // sign autofill color
-// min width naproducts
-// scroll to top symbol
-// da opravq errorite, moje samo s 1
 
 // klient da si zibra kakvo info da se vijda za nego
-
-// <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" rel="stylesheet">
-// <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-// mat-typography
 
 // ng build --prod --base-href "https://web-portfolio-angular.github.io"
 // ngh --dir=dist/index
